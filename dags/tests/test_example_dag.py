d6f3100570301f2bd712b96612115ef00402c2c9
fanzi19 <143977747+fanzi19@users.noreply.github.com>
2025-02-23 22:48:19 -0600

    fix the approach to test the DAG structure without requiring the database

--- dags/tests/test_example_dag.py
+++ dags/tests/test_example_dag.py
@@ -1,24 +1,32 @@
 from airflow.models import DagBag
 import pytest
 
-def test_dag_loaded(dagbag):
-    dag = dagbag.get_dag(dag_id='example_dag')
-    assert dagbag.import_errors == {}
+def test_dag_loaded():
+    """Test that the DAG file can be imported"""
+    dagbag = DagBag(dag_folder='dags', include_examples=False)
+    assert len(dagbag.import_errors) == 0, f"DAG import errors: {dagbag.import_errors}"
+    assert 'example_dag' in dagbag.dags
+
+def test_dag_structure():
+    """Test the structure of the DAG"""
+    dagbag = DagBag(dag_folder='dags', include_examples=False)
+    dag = dagbag.dags['example_dag']
     assert dag is not None
-    assert len(dag.tasks) == 1
-
-def test_dag_structure(dagbag):
-    dag = dagbag.get_dag(dag_id='example_dag')
+    # Test tasks
     task_ids = [task.task_id for task in dag.tasks]
-    assert task_ids == ['print_hello']
+    assert 'print_hello' in task_ids
 
-def test_dag_defaults(dagbag):
-    dag = dagbag.get_dag(dag_id='example_dag')
+def test_dag_defaults():
+    """Test the default arguments of the DAG"""
+    dagbag = DagBag(dag_folder='dags', include_examples=False)
+    dag = dagbag.dags['example_dag']
     assert dag.default_args['owner'] == 'airflow'
     assert dag.default_args['retries'] == 1
 
-def test_dependencies(dagbag):
-    dag = dagbag.get_dag(dag_id='example_dag')
+def test_dependencies():
+    """Test task dependencies"""
+    dagbag = DagBag(dag_folder='dags', include_examples=False)
+    dag = dagbag.dags['example_dag']
     task = dag.get_task('print_hello')
-    assert len(task.upstream_list) == 0
-    assert len(task.downstream_list) == 0
+    assert len(task.upstream_list) == 0  # No upstream tasks
+    assert len(task.downstream_list) == 0  # No downstream tasks
